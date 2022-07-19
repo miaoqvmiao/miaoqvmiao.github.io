9a96e58902443df9116b63e80a9b9e6390fd8b3c
miaoqvmiao <99528572+miaoqvmiao@users.noreply.github.com>
2022-07-19 11:13:34 +0800

    Update index.js

--- game/CALC/index.js
+++ game/CALC/index.js
@@ -1,20 +1,32 @@
 var wt = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
     answer,
     q1,
-    q2
+    q2,
+    max = 100,
+    min = -100,
+    speed = 2
 
 function setup(){
-  if (navigator.userAgent.indexOf('Android') == -1 || navigator.userAgent.indexOf('iPhone') == -1){
-    createCanvas(640,640)
-  }else{
-    createCanvas(wt,wt)
-  }
+    if (navigator.userAgent.indexOf('Android') == -1 || navigator.userAgent.indexOf('iPhone') == -1){
+      createCanvas(640,640)
+    }else{
+      createCanvas(wt,wt)
+    }
 }
 
 function draw(){
-  background(0)
+    background(0)
 }
 
-function random(){
-  q1 = Math.ceil(Math.random()*99)
+function randQ(){
+    q1 = Math.floor(Math.random()*(max-min+1)+min)
+    q2 = Math.floor(Math.random()*(q1-min)+min)
+    answer = q1 + q2
+}
+
+function move(){
+    var a = document.getElementsByClassName("answer")
+    for (let i in a){
+        a[i].style.top += speed
+    }
 }
