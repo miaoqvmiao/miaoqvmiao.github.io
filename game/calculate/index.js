e47dc103790b2a7e1aca6c2be906c3b7b0d6f0ad
miaoqvmiao <99528572+miaoqvmiao@users.noreply.github.com>
2022-07-19 08:53:02 +0800

    Update index.js

--- game/calculate/index.js
+++ game/calculate/index.js
@@ -1,1 +1,13 @@
+var wt = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
 
+function setup(){
+  if (navigator.userAgent.indexOf('Android') == -1){
+    createCanvas(640,640)
+  }else{
+    createCanvas(wt,wt)
+  }
+}
+
+function draw(){
+  background(0)
+}
