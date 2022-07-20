0b932bdcddbf7e71c74c2a01d3b26741e60ee66d
miaoqvmiao <99528572+miaoqvmiao@users.noreply.github.com>
2022-07-20 09:46:11 +0800

    Update index.js

--- game/CALC/index.js
+++ game/CALC/index.js
@@ -12,7 +12,7 @@ var wt = window.innerWidth || document.documentElement.clientWidth || document.b
     place
 
 function setup(){
-    if (navigator.userAgent.indexOf('Android') == -1 || navigator.userAgent.indexOf('iPhone') == -1){
+    if (navigator.userAgent.indexOf('Android') == -1 && navigator.userAgent.indexOf('iPhone') == -1){
       createCanvas(640,640)
     }else{
       createCanvas(wt,wt)
