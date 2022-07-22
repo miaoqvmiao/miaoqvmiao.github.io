a817db63f741766e2043d124f7bddb57b496db5d
miaoqvmiao <99528572+miaoqvmiao@users.noreply.github.com>
2022-07-22 09:49:54 +0800

    Update index.js

--- game/CALC/index.js
+++ game/CALC/index.js
@@ -113,9 +113,9 @@ function hit(b,q){
 class question{
     constructor(nr){
         this.nr = nr
-        this.y = 0
         this.w = 90
         this.h = 90
+        this.y = this.h - this.h * 2
         this.x = Math.ceil(Math.random() * wt) - this.w /2
         ques.push(this)
     }
